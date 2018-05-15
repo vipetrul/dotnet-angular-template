8f4642d94d3a6b0250d84c9491fca5273ad9cfdc
Valerij Petrulevich <valerij-petrulevich@uiowa.edu>
2018-05-15 16:27:22 -0500

    toolbar shadow will not show always

--- ServiceCatalog.Web/angular/src/app/core/components/login-button/login-button.component.ts
+++ ServiceCatalog.Web/angular/src/app/core/components/login-button/login-button.component.ts
@@ -7,7 +7,7 @@ import { Select } from '@ngxs/store';
   selector: 'login-button',
   template: `
     <a mat-button *ngIf="!(isAuthenticated$ | async)" href="account/Login">Login</a>
-    <a mat-button *ngIf="(isAuthenticated$ | async)" fxHide.xs>{{(user$ | async).hawkId}}<mat-icon svgIcon="arrow_drop_down"></mat-icon></a>
+    <a mat-button *ngIf="(isAuthenticated$ | async)" fxHide.xs><mat-icon svgIcon="person"></mat-icon>{{(user$ | async).hawkId}}</a>
     <a mat-button mat-icon-button *ngIf="(isAuthenticated$ | async)" fxHide.gt-xs><mat-icon svgIcon="person"></mat-icon></a>
   `,
   styles: []
