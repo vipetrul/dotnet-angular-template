aa667d3ca3963a094ec3dc17740df82dc1bdf836
Valerij Petrulevich <valerij-petrulevich@uiowa.edu>
2018-05-15 09:02:35 -0500

    Two version of logged in button, xsmall and on larger screens

--- ServiceCatalog.Web/angular/src/app/core/components/login-button/login-button.component.ts
+++ ServiceCatalog.Web/angular/src/app/core/components/login-button/login-button.component.ts
@@ -7,7 +7,8 @@ import { Select } from '@ngxs/store';
   selector: 'login-button',
   template: `
     <a mat-button *ngIf="!(isAuthenticated$ | async)" href="account/Login">Login</a>
-    <a mat-button *ngIf="(isAuthenticated$ | async)"><mat-icon svgIcon="person"></mat-icon>{{(user$ | async).hawkId}}</a>
+    <a mat-button *ngIf="(isAuthenticated$ | async)" fxHide.xs><mat-icon svgIcon="person"></mat-icon>{{(user$ | async).hawkId}}</a>
+    <a mat-button mat-icon-button *ngIf="(isAuthenticated$ | async)" fxHide.gt-xs><mat-icon svgIcon="person"></mat-icon></a>
   `,
   styles: []
 })
